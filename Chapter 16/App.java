ee753707a8291c3d75a9e3eee07fc2eedac7b181
HThacker03 <thacker.han0353@stu.stech.edu>
2024-11-13 10:23:06 -0700

    Add files via upload

--- Chapter 16/App.java
+++ Chapter 16/App.java
@@ -1,107 +1,87 @@
 package Demo;
 import javafx.application.Application;
-//import javafx.event.ActionEvent;
-//import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+//import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.paint.Color;
+//import javafx.scene.control.RadioButton;
+//import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCode;
+//import javafx.scene.paint.Color;
 import javafx.geometry.Pos;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
+//import javafx.scene.layout.HBox;
+//import javafx.scene.layout.Pane;
+//import javafx.scene.text.Text;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+//import javafx.scene.text.FontWeight;
+//import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
- 
+
 public class App extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+    private final static String MEDIA_URL = "https://liveexample.pearsoncmg.com/common/audio/anthem/anthem0.mp3";
 
-    
-    @Override
-    public void start(Stage primaryStage) {
-
-     Text text = new Text (150, 80, "Programming is fun :)");
-      text.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
-     BorderPane pane = new BorderPane();
-        HBox paneForButtons = new HBox(20);
-        Button btLeft = new Button("<=");
-        Button btRight = new Button("=>");
-        paneForButtons.getChildren().addAll(btLeft, btRight);
-        paneForButtons.setAlignment(Pos.CENTER);
-        paneForButtons.setStyle("-fx-border-color: green");
-
-        pane.setBottom(paneForButtons);
-
-        Pane paneForText = new Pane();
-        paneForText.getChildren().add(text);
-        pane.setCenter(paneForText);
-
-        btLeft.setOnAction(e -> text.setX(text.getX() > 0 ? text.getX() - 10 : 0));
-        btRight.setOnAction(e -> text.setX(text.getX() < 310 ? text.getX() + 10 : 310));
+   public static void main(String[] args) {
+       launch(args);
+   }
+  
+  
+   @Override
+   public void start(Stage primaryStage) {
 
 
-        //Radio buttons
-        HBox paneForRadioButtons = new HBox(20);
-        paneForRadioButtons.setPadding(new Insets(5, 5, 5, 5));
-        paneForRadioButtons.setStyle("-fx-border-width: 2px; -fx-border-color: green");
+    BorderPane pane = new BorderPane();
+    pane.setPadding(new Insets(5, 5, 5, 5));
 
-        RadioButton rbRed = new RadioButton("Red");
-        RadioButton rbYellow = new RadioButton("Yellow");
-        RadioButton rbBlack = new RadioButton("Black");
-        RadioButton rbOrange = new RadioButton("Orange");
-        RadioButton rbGreen = new RadioButton("Green");
-        
-        paneForRadioButtons.getChildren().addAll(rbRed, rbYellow, rbBlack, rbOrange, rbGreen);
-        pane.setTop(paneForRadioButtons);
 
-        ToggleGroup group = new ToggleGroup();
-        rbRed.setToggleGroup(group);
-        rbYellow.setToggleGroup(group);
-        rbBlack.setToggleGroup(group);
-        rbOrange.setToggleGroup(group);
-        rbGreen.setToggleGroup(group);
+    TextField tf = new TextField();
+    tf.setEditable(true);
+    tf.setFont(Font.font("Times", 40));
+    tf.setAlignment(Pos.CENTER);
+    pane.setCenter(tf);
 
-        rbRed.setOnAction (e -> {
-            if (rbRed.isSelected()) {
-                text.setFill(Color.RED);
-            }
-        });
+    //private final static String MEDIA_URL = "https://liveexample.pearsoncmg.com/common/audio/anthem/anthem0.mp3";
+    Media media = new Media(MEDIA_URL);
+    MediaPlayer mediaPlayer = new MediaPlayer(media);
 
-        rbBlack.setOnAction (e -> {
-            if (rbBlack.isSelected()) {
-                text.setFill(Color.BLACK);
-            }
-        });
+    EventHandler<ActionEvent> eventHandler = e -> {
+        Integer num = Integer.parseInt(tf.getText());
+        if(num > 0) {
+            num = num - 1;
+            tf.setText(Integer.toString(num));
+            //System.out.println(num);
+        }
+        else if(num == 0) {
+            mediaPlayer.play();
+        }
+    };
 
-        rbYellow.setOnAction (e -> {
-            if (rbYellow.isSelected()) {
-                text.setFill(Color.YELLOW);
-            }
-        });
+    Timeline animation = new Timeline(new KeyFrame(Duration.millis(1000), eventHandler));
+    animation.setCycleCount(Timeline.INDEFINITE);
 
-        rbOrange.setOnAction (e -> {
-            if (rbOrange.isSelected()) {
-                text.setFill(Color.ORANGE);
-            }
-        });
 
-        rbGreen.setOnAction (e -> {
-            if (rbGreen.isSelected()) {
-                text.setFill(Color.GREEN);
-            }
-        });
 
-        Scene scene = new Scene(pane, 500, 200);
-        primaryStage.setTitle("Exercise16_01");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+    tf.setOnKeyPressed(e -> {
+       if (e.getCode() == KeyCode.ENTER) {
+           //tf.setStyle("-fx-text-fill: green");
+           animation.play();
+       }
+    });
+
+
+
+
+       Scene scene = new Scene(pane, 200, 200);
+       primaryStage.setTitle("Exercise16_21");
+       primaryStage.setScene(scene);
+       primaryStage.show();
+   }
 }
